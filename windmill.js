201457d91479420c704d4ae487286c915da5aca3
Aitor Fernandez <aitor@qad.re>
2019-08-29 10:34:47 +0100

    rotate

--- windmill.js
+++ windmill.js
@@ -1,11 +1,39 @@
 export default (p) => {
+  function getWidth() {
+    return p.windowWidth
+  }
+
+  function getHeight() {
+    return p.windowHeight
+  }
 
   function setup() {
-    p.noLoop()
+    p.createCanvas(getWidth(), getHeight())
+
+    p.frameRate(1)
   }
 
   function draw() {
-    console.log('draw')
+    p.background(255)
+
+    p.push()
+    p.translate(getWidth() / 2, getHeight() / 2)
+    p.rotate(p.frameCount / 500)
+    polygon(0, 0, 50, 2)
+    p.pop()
+  }
+
+  function polygon(x, y, radius, vertices) {
+    let angle = p.TWO_PI / vertices
+
+    p.beginShape()
+    for (let i = 0; i < p.TWO_PI; i += angle) {
+      let vx = x + p.cos(i) * radius
+      let vy = y + p.sin(i) * radius
+
+      p.vertex(vx, vy)
+    }
+    p.endShape(p.CLOSE)
   }
 
   p.setup = setup
