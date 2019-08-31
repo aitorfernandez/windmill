b57e195805e41caa62c9726cea36d4aa7d654419
Aitor <helloimaitor@gmail.com>
2019-09-01 00:05:10 +0100

    distances and points

--- windmill.js
+++ windmill.js
@@ -1,39 +1,101 @@
 export default (p) => {
-  function getWidth() {
-    return p.windowWidth
+  const points = []
+
+  const len = 600
+
+  function circle({ x, y }) {
+    p.fill('white')
+    p.circle(x, y, 10)
   }
 
-  function getHeight() {
-    return p.windowHeight
+  function createPoint() {
+    return Object.assign(
+      p.createVector(
+        p.int(p.random(p.width - 100)),
+        p.int(p.random(p.height - 100))
+      ),
+      { isPivot: false }
+    )
+  }
+
+  function findPivot() {
+    return points.find((point) => point.isPivot)
+  }
+
+  function checkDist(a, b, c) {
+    const dist = (a, b) => p.dist(a.x, a.y, b.x, b.y)
+
+    const d1 = dist(a, c)
+    const d2 = dist(b, c)
+    const d3 = dist(a, b)
+
+    const round = (value) => Math.round(value * 100) / 100
+
+    return (
+      round(d1 + d2) === round(d3)
+      // TODO: Review this later...
+      || round(d1 + d2) < (round(d3) + 0.02)
+      || round(d1 + d2) < (round(d3) + 0.04)
+      || round(d1 + d2) < (round(d3) + 0.06)
+    )
+  }
+
+  function removePivot() {
+    points.forEach((point) => {
+      point.isPivot = false
+    })
   }
 
   function setup() {
-    p.createCanvas(getWidth(), getHeight())
+    p.createCanvas(len, len)
 
-    p.frameRate(1)
+    for (let i = 0; i < 4; i += 1) {
+      points.push(createPoint())
+    }
+
+    points[p.floor(p.random() * points.length)].isPivot = true
+
+    p.frameRate(24)
+
+    // p.noLoop()
   }
 
   function draw() {
-    p.background(255)
+    p.background('lightGray')
+
+    points.forEach((point) => {
+      circle(point)
+    })
+
+    const pivot = findPivot()
 
     p.push()
-    p.translate(getWidth() / 2, getHeight() / 2)
-    p.rotate(p.frameCount / 500)
-    polygon(0, 0, 50, 2)
+    p.translate(pivot.x, pivot.y)
+
+    const radians = p.radians(p.frameCount)
+
+    p.rotate(radians)
+    p.line(len * -1, 0, len, 0)
     p.pop()
-  }
 
-  function polygon(x, y, radius, vertices) {
-    let angle = p.TWO_PI / vertices
+    let sp = null
+    let ep = null
 
-    p.beginShape()
-    for (let i = 0; i < p.TWO_PI; i += angle) {
-      let vx = x + p.cos(i) * radius
-      let vy = y + p.sin(i) * radius
+    sp = p5.Vector.fromAngle(radians, len).add(pivot.x, pivot.y)
+    ep = p5.Vector.fromAngle(radians, len).mult(-1).add(pivot.x, pivot.y)
 
-      p.vertex(vx, vy)
+    for (
+      let i = 0, length = points.length; i < length; i += 1
+    ) {
+      if (!points[i].isPivot) {
+        const distance = checkDist(sp, ep, points[i])
+        if (distance) {
+          removePivot()
+          points[i].isPivot = true
+          break
+        }
+      }
     }
-    p.endShape(p.CLOSE)
   }
 
   p.setup = setup
